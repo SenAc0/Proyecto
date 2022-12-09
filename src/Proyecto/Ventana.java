a19d1462b3128a50be421a705ebfddd37a914f8c
NeekoSakata <114708095+NeekoSakata@users.noreply.github.com>
2022-12-09 12:35:16 -0300

    Update Ventana.java
    Añadida funcion cambiar entre pizarras

--- src/Proyecto/Ventana.java
+++ src/Proyecto/Ventana.java
@@ -2,19 +2,24 @@ package Proyecto;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 class Ventana extends JFrame{
-    Panel p1 = new Panel();
-    Panel p2 = new Panel();
-    PanelDeControl p3 = new PanelDeControl();
-    Panel contenedor;
+    Panel p1;
+    Panel p2 ;
+    PanelDeControl p3;
+    JPanel contenedor = new JPanel(new BorderLayout());
     public Ventana(){
         this.setLayout(new BorderLayout());
-        contenedor = p1;
         
+        p1 = new Panel();
+        p2 = new Panel();
+        p3 = new PanelDeControl(contenedor,p1,p2);
+        contenedor.add(p1,BorderLayout.CENTER);
         add(contenedor,BorderLayout.CENTER); //se agrega al centro
         add(p3,BorderLayout.NORTH); //se agrega al centro
+        
         
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);//activar cierre en la X 
         this.setSize(1000,800); //se pueden pasar como parámetros del constructor
